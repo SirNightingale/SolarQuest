47daecee935559c2601d8a30391671817b09a792
SirNightingale <dm1solovyov@mail.ru>
2020-09-18 15:51:37 +0600

    fix paths to web resources for showing on difference devices

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require('express'),
     app = express(),
+    path = require('path');
     port = Number(process.env.PORT || 8080);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
 	res.sendFile('index.html', {root: __dirname});
 });
 
 app.listen(port, function() {
-   console.log('Listening on port ' + port);
+   console.log('Listening on port ' + port + '; root: ' + __dirname);
 });
--- public/script/main.js
+++ public/script/main.js
@@ -1,15 +1,9 @@
-alert(1);
 let type = "WebGL";
-alert(2);
 try {
 if(!PIXI.utils.isWebGLSupported()){
-alert(3);
 type = "canvas";
-alert(4);
 }
 } catch (e) {
 alert(e)
 }
-alert(5);
 PIXI.utils.sayHello(type);
-alert(6);
